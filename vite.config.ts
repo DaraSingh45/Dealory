33fb46bb0065e62beb7fa3c3011d893f4d01c11e
DaraSingh45 <thedarasingh45@gmail.com>
2026-07-31 20:53:52 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import { tanstackStart } from "@tanstack/start/plugin/vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [
+    tsconfigPaths(),
+    tailwindcss(),
+    react(),
+    tanstackStart({
+      server: {
+        entry: "server",
+      },
+    }),
+  ],
+});
